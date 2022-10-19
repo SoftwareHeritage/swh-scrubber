17cf8b668739cf0719a624ebeeef1608d4230b30
Antoine Lambert <anlambert@softwareheritage.org>
2022-10-19 14:59:39 +0200

    sql: Fix typos detected by codespell

--- swh/scrubber/sql/30-schema.sql
+++ swh/scrubber/sql/30-schema.sql
@@ -31,7 +31,7 @@ create table checked_range
   last_date             timestamptz not null
 );
 
-comment on table checked_range is 'Each row represents a range of objects in a datastore that were fetched, checksumed, and checked at some point in the past.';
+comment on table checked_range is 'Each row represents a range of objects in a datastore that were fetched, checksummed, and checked at some point in the past.';
 comment on column checked_range.range_start is 'First SWHID of the range that was checked (inclusive, possibly non-existent).';
 comment on column checked_range.range_end is 'Last SWHID of the range that was checked (inclusive, possiby non-existent).';
 comment on column checked_range.last_date is 'Date the last scrub of that range *started*.';
