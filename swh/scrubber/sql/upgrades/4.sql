5432f51d5a061d52f4804bbd35520e8df62f6cd1
Antoine Lambert <anlambert@softwareheritage.org>
2024-08-30 17:43:07 +0200

    sql/upgrades: Fix typo spotted after codespell upgrade

--- swh/scrubber/sql/upgrades/4.sql
+++ swh/scrubber/sql/upgrades/4.sql
@@ -14,7 +14,7 @@ create table checked_range
 
 comment on table checked_range is 'Each row represents a range of objects in a datastore that were fetched, checksummed, and checked at some point in the past.';
 comment on column checked_range.range_start is 'First SWHID of the range that was checked (inclusive, possibly non-existent).';
-comment on column checked_range.range_end is 'Last SWHID of the range that was checked (inclusive, possiby non-existent).';
+comment on column checked_range.range_end is 'Last SWHID of the range that was checked (inclusive, possibly non-existent).';
 comment on column checked_range.last_date is 'Date the last scrub of that range *started*.';
 
 create unique index concurrently checked_range_pkey on checked_range(datastore, range_start, range_end);
