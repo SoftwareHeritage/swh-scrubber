a6e90620c35645527f4c465818c9d6d3ff3bb10f
David Douard <david.douard@sdfa3.org>
2024-10-11 14:31:27 +0200

    Update for swh.core 3.6
    Update the scrubber db for better `swh db` compatibility and
    update test_migration for swh.core 3.6 (acually 3.6.1 is needed).

--- swh/scrubber/db.py
+++ swh/scrubber/db.py
@@ -1,8 +1,7 @@
-# Copyright (C) 2022-2023  The Software Heritage developers
+# Copyright (C) 2022-2024  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
-
 
 import dataclasses
 import datetime
@@ -77,6 +76,13 @@ class FixedObject:
 
 class ScrubberDb(BaseDb):
     current_version = 7
+
+    def __init__(self, db, **kwargs):
+        if isinstance(db, str):
+            conn = psycopg2.connect(db, **kwargs)
+        else:
+            conn = db
+        super().__init__(conn=conn)
 
     ####################################
     # Shared tables
